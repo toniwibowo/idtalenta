9723e955903732824be81990b6751ae5f4aa8c0b
gravenza <toniewibowo@gmail.com>
2020-07-13 06:54:53 +0700

    update nav carousel

--- js/main.js
+++ js/main.js
@@ -133,6 +133,7 @@
     $('.main-slider-active-3').owlCarousel({
         loop: true,
         nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
         dots: true,
         autoplay: false,
         autoplayTimeout: 5000,
